9d1721ce217c08b4ab807537801376bbfae8c0d5
Ashwitha <ashwitha.b.aux@go-jek.com>
2019-06-02 21:13:53 +0530

    Add async call to get shops

--- src/containers/StoreLocator.js
+++ src/containers/StoreLocator.js
@@ -1,41 +1,35 @@
 import React from 'react';
+import axios from 'axios';
+
 import Button from "../components/Button";
 import Map from "../components/Map";
 import Header from '../components/Header';
 import mapChooser from "../mapChooser";
+
 import '../App.css';
 
 class StoreLocator extends React.Component {
-    stores = [
-        {
-            'location': 'Portland',
-            'address': '123 Portland Dr'
-        },
-        {
-            'location': 'Astoria',
-            'address': '123 Astoria Dr'
-        },
-        {
-            'location': '',
-            'address': ''
-        }];
-
-
     constructor(props) {
         super(props);
         this.state = {
-            currentMap: 'none.png'
+            currentMap: 'none.png',
+            shops: []
         };
-        this.shops = this.stores;
+
         this.chooseMap = this.chooseMap.bind(this);
     }
 
-    chooseMap(event){
+    async componentDidMount() {
+        let response = await axios.get('http://localhost:3000/data/shops.json');
+        this.setState({shops: response.data.shops});
+    }
+
+    chooseMap(event) {
         this.setState({currentMap: mapChooser(event.target.value)});
     }
 
     render() {
-        let storeButtons = this.shops.map((shop, id) => {
+        let storeButtons = this.state.shops.map((shop, id) => {
             return (<Button handleClick={this.chooseMap} key={id} location={shop.location}/>)
         });
 
@@ -43,7 +37,7 @@ class StoreLocator extends React.Component {
             <div>
                 <Header/>
                 <div> {storeButtons} </div>
-                <Map imageName = {this.state.currentMap} location={this.props.location}/>
+                <Map imageName={this.state.currentMap} location={this.props.location}/>
             </div>
         )
     }
--- src/containers/__tests__/StoreLocator.test.js
+++ src/containers/__tests__/StoreLocator.test.js
@@ -1,6 +1,7 @@
 import React from 'React';
 import {shallow} from 'enzyme';
 import StoreLocator from '../StoreLocator';
+import axios from 'axios';
 
 describe('storeLocator', function () {
     let mountedStoreLocator;
@@ -9,16 +10,38 @@ describe('storeLocator', function () {
         mountedStoreLocator = shallow(<StoreLocator/>)
     });
 
+    it('calls axios.get in #componentDidMount', function () {
+        return mountedStoreLocator.instance().componentDidMount().then(() => {
+            expect(axios.get).toHaveBeenCalled();
+        })
+    });
+
+    it('calls axios.get with correct url', function () {
+        return mountedStoreLocator.instance().componentDidMount().then(() => {
+            expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/data/shops.json');
+        })
+    });
+
+    it('updates state after AJAX call', function () {
+        return mountedStoreLocator.instance().componentDidMount().then(() => {
+            expect(mountedStoreLocator.state()).toHaveProperty('shops',
+                [{
+                    "location": "test location",
+                    "address": "test address"
+                }])
+        })
+    });
+
     it('renders Header', function () {
         const mountedHeader = mountedStoreLocator.find('Header');
 
         expect(mountedHeader.length).toBe(1)
     });
 
-    it('renders two Buttons', function () {
+    it('renders zero Buttons initially', function () {
         const mountedButton = mountedStoreLocator.find('Button');
 
-        expect(mountedButton.length).toBe(3)
+        expect(mountedButton.length).toBe(0)
     });
 
     it('renders Map', function () {
